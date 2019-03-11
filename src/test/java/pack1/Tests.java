996ce1db57868f0e9b338de1784bc59d001f620a
bhushan <bhushan.allatipalli@imaginea.com>
2019-03-11 19:57:37 +0530

    Added Click on register step in tests

--- src/test/java/pack1/Tests.java
+++ src/test/java/pack1/Tests.java
@@ -20,6 +20,8 @@ public class Tests
 		  reuse.waitFor(4);
 		  reuse.navigateUrl("http://newtours.demoaut.com/",logger);
 		  reuse.waitFor(4);
+		  reuse.clickElement("//a[text()='REGISTER']", "Register", logger);
+		  reuse.waitFor(4);
 		  reuse.closeBrowser(logger);
 		  reuse.finishAndSaveReport(logger);
 	  }
